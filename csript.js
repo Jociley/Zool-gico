43b0b982d5d6157ca7148168edb3c27dce405482
Jociley dos Santos Moreira Gomes <248862024@eniac.edu.br>
2025-09-12 16:24:35 -0300

    subindo projeto

---
+++ csript.js
@@ -0,0 +1,28 @@
+// Ano no rodapé
+
+document.getElementById('2025').textContent = new Date().getFullYear();
+
+// Botão "voltar ao topo"
+document.addEventListener('DOMContentLoaded',()=>{})
+const toTop = document.getElementById('toTop');
+if (!btn) return;
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 400) toTop.classList.add('visivel');
+  else toTop.classList.remove('visivel');
+});
+toTop.addEventListener('click', () => window.scrollTo({ top: 0, behavior: 'smooth' }));
+
+// Formulário (simulação)
+const form = document.getElementById('form-contato');
+const statusEl = document.getElementById('msg-status');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const dados = new FormData(form);
+  const nome = dados.get('nome');
+  statusEl.textContent = 'Enviando...';
+  setTimeout(() => {
+    statusEl.textContent = `Obrigada(o), ${Jociley}! Recebi sua mensagem.`;
+    form.reset();
+  }, 800);
+});
